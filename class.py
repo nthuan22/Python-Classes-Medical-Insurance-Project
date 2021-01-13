cc2d39c9a23ce3e2f5cfb2f1e0db74b1b7590b45
nthuan22 <53529609+nthuan22@users.noreply.github.com>
2021-01-13 13:22:16 +0700

    Create class.py

---
+++ class.py
@@ -0,0 +1,57 @@
+#patient constructor
+class Patient:
+  def __init__(self, name, age, sex, bmi, num_of_children, smoker):
+    self.name = name
+    self.age = age
+    self.sex = sex
+    self.bmi = bmi
+    self.num_of_children = num_of_children
+    self.smoker = smoker
+  
+  #insurance costs method
+  def estimated_insurance_cost(self):
+    estimated_cost = (250 * self.age) - (128 * self.sex) + (370 * self.bmi) + (425 * self.num_of_children) + (24000 * self.smoker) - 12500
+    print(self.name + "'s estimated insurance costs is " + str(estimated_cost) + " dollars.")
+  
+
+  #update age
+  def update_age(self, new_age):
+    self.age = new_age
+    print(self.name + " is now " + str(self.age) + " years old.")
+    self.estimated_insurance_cost()
+  
+  #update number of children
+  def update_num_children(self, new_num_children):
+    self.num_of_children = new_num_children
+    if self.num_of_children == 1:
+      print(self.name + " has " + str(self.num_of_children) + " child.")
+    else:
+      print(self.name + " has " + str(self.num_of_children) + " children.")
+      self.estimated_insurance_cost()
+    
+  #patient profile
+  def patient_profile(self):
+    patient_information = {}
+    patient_information["Name"] = self.name
+    patient_information["Age"] = self.age
+    patient_information["Sex"] = self.sex
+    patient_information["BMI"] = self.bmi
+    patient_information["Number of Children"] = self.num_of_children
+    patient_information["Smoker"] = self.smoker
+    return patient_information
+
+    
+
+
+
+#print out information
+patient1 = Patient("John Doe", 25, 1, 22.2, 0, 0)
+print(patient1.name)
+patient1.estimated_insurance_cost()
+patient1.update_age(26)
+patient1.update_num_children(5)
+print(patient1.patient_profile())
+
+
+
+ 
